844386de6166fff5b00c9f86bf8097b5b9c74978
anasta1shn <166626423+anasta1shn@users.noreply.github.com>
2025-12-20 00:08:47 +0400

    Create DatabaseSeeder.php

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Role;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        Role::create(['title' => 'Администратор']);
+        Role::create(['title' => 'Пользователь']);
+
+        $this->call(UsersTableSeeder::class);
+    }
+}
